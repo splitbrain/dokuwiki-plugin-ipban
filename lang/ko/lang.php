03375d8a051975999f64a3a0523ec799a19ce26b
Dryoo <dryoo@live.com>
2013-07-28 02:09:42 +0200

    Korean translation added

---
+++ lang/ko/lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['menu'] = 'IP 차단 관리자';
+
+$lang['ip']     = 'IP 주소';
+$lang['host']   = 'Host Name';
+$lang['date']   = '날짜';
+$lang['by']     = '차단한 사람';
+$lang['cause']  = '설명';
+$lang['del']    = '삭제';
+$lang['ban']    = '차단';
+$lang['newban'] = '새 차단 추가';
+
+$lang['denied'] = '접근금지됨';
+$lang['banned'] = '당신의 IP 주소 (%s) 는 %s 에서 다음과 같은 이유로 차단되었습니다:<br /><i>%s</i>';
