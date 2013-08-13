5a552fe52ce25ed7cba68b4a7c9eef730034cb87
Myeongjin <aranet100@gmail.com>
2013-08-13 18:30:20 +0200

    translation update

--- lang/ko/lang.php
+++ lang/ko/lang.php
@@ -15,4 +15,4 @@ $lang['del']                   = '삭제';
 $lang['ban']                   = '차단';
 $lang['newban']                = '새 차단 추가';
 $lang['denied']                = '접근 금지됨';
-$lang['banned']                = '당신의 IP(%s)는 %s 에서 다음 이유로 차단되었습니다:<br /><i>%s</i>';
+$lang['banned']                = '당신의 IP(%s)는 %s에서 다음 이유로 차단되었습니다:<br /><i>%s</i>';
