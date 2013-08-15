26c7fb7c340f7c67349797318daf44cff58c43f2
Tim Weinhold <tim.weinhold@gmail.com>
2013-08-15 11:05:06 +0200

    translation update

---
+++ lang/de/lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Tim Weinhold <tim.weinhold@gmail.com>
+ */
+$lang['menu']                  = 'IP-Sperren Manager';
+$lang['ip']                    = 'IP-Adresse';
+$lang['host']                  = 'Hostname';
+$lang['date']                  = 'Datum';
+$lang['by']                    = 'Gesperrt von';
+$lang['cause']                 = 'Begründung';
+$lang['del']                   = 'Löschen';
+$lang['ban']                   = 'Sperren';
+$lang['newban']                = 'Neue Sperre hinzufügen';
+$lang['denied']                = 'Zugriff abgelehnt';
+$lang['banned']                = 'Ihre IP (%s) wurde am %s wegen folgendem Grund gesperrt:<br><i>%s</i>';
