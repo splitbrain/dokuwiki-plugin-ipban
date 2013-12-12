3a0dd57421b93f2aba8ad83e1f5b804a9ee0c1ab
bruno <bruno@ninesys.fr>
2013-12-12 23:10:15 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author bruno <bruno@ninesys.fr>
+ */
+$lang['ip']                    = 'Adresse IP';
+$lang['host']                  = 'Nom d\'hôte';
+$lang['date']                  = 'Date';
+$lang['by']                    = 'Banni par';
+$lang['del']                   = 'Effacer';
+$lang['ban']                   = 'Ban';
+$lang['newban']                = 'Ajouter un nouveau ban';
+$lang['denied']                = 'Accès Refusé';
+$lang['banned']                = 'Votre adresse IP (%s) a été bannie par %s pour la raison suivante : <br /><i>%s</i>';
