e46b61cb937b20a9ea1dd8960d071de04101d35a
Andreas Gohr <andi@splitbrain.org>
2016-02-02 16:08:24 +0100

    Adjust method signatures to match parent

--- action.php
+++ action.php
@@ -14,7 +14,7 @@ class action_plugin_ipban extends DokuWiki_Action_Plugin {
     /**
      * register the eventhandlers and initialize some options
      */
-    function register(&$controller){
+    function register(Doku_Event_Handler $controller){
 
         $controller->register_hook('DOKUWIKI_STARTED',
                                    'BEFORE',
