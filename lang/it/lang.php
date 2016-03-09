72ffd051374f33bb8a75fa1d2e4cfc872219fc97
Sebastiano Pistore <olatusrooc@virgilio.it>
2016-03-09 11:20:32 +0100

    translation update

---
+++ lang/it/lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Sebastiano Pistore <olatusrooc@virgilio.it>
+ */
+$lang['menu']                  = 'Gestione dei ban degli IP';
+$lang['ip']                    = 'Indirizzo IP';
+$lang['host']                  = 'Nome dell\'host';
+$lang['date']                  = 'Data';
+$lang['by']                    = 'Bannato da';
+$lang['cause']                 = 'Motivazione';
+$lang['del']                   = 'Cancella';
+$lang['ban']                   = 'Ban';
+$lang['newban']                = 'Aggiungi nuovo ban';
+$lang['denied']                = 'Accesso negato';
+$lang['banned']                = 'Il tuo IP (%s) è stato bannato il %s per la seguente motivazione:<br /><i>%s</i>';
