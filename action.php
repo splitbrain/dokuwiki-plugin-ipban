b569fb4a8bdb308efb7c4beac5b9a20f33cfade1
Andreas Gohr <andi@splitbrain.org>
2019-07-31 20:49:26 +0200

    basic cleanup (PSR-2)

--- action.php
+++ action.php
@@ -4,42 +4,45 @@
  * @author     Andreas Gohr <andi@splitbrain.org>
  */
 
-// must be run within Dokuwiki
-if(!defined('DOKU_INC')) die();
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-require_once(DOKU_PLUGIN.'action.php');
-
-class action_plugin_ipban extends DokuWiki_Action_Plugin {
+class action_plugin_ipban extends DokuWiki_Action_Plugin
+{
 
     /**
      * register the eventhandlers and initialize some options
      */
-    function register(Doku_Event_Handler $controller){
+    public function register(Doku_Event_Handler $controller)
+    {
 
         $controller->register_hook('DOKUWIKI_STARTED',
-                                   'BEFORE',
-                                   $this,
-                                   'handle_start',
-                                   array());
+            'BEFORE',
+            $this,
+            'handle_start',
+            array());
     }
 
     /**
      * Do the magic
+     *
+     * @param Doku_Event $event
+     * @param $param
      */
-    function handle_start(&$event, $param){
+    public function handle_start(Doku_Event $event, $param)
+    {
         global $conf;
-        $bans = @file($conf['cachedir'].'/ipbanplugin.txt');
+        $bans = @file($conf['cachedir'] . '/ipbanplugin.txt');
         $client = clientIP(true);
-        if(is_array($bans)) foreach($bans as $ban){
-            $fields = explode("\t",$ban);
-            if($fields[0] == $client){
+        if (!is_array($bans)) return;
+
+        foreach ($bans as $ban) {
+            $fields = explode("\t", $ban);
+            if ($fields[0] == $client) {
                 $text = $this->locale_xhtml('banned');
-                $text .= sprintf('<p>'.$this->getLang('banned').'</p>',
-                                 hsc($client), strftime($conf['dformat'],$fields[1]),
-                                 hsc($fields[3]));
+                $text .= sprintf('<p>' . $this->getLang('banned') . '</p>',
+                    hsc($client), strftime($conf['dformat'], $fields[1]),
+                    hsc($fields[3]));
                 $title = $this->getLang('denied');
                 header("HTTP/1.0 403 Forbidden");
-                echo<<<EOT
+                echo <<<EOT
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
 "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -56,5 +59,4 @@ EOT;
             }
         }
     }
-
 }
--- admin.php
+++ admin.php
@@ -1,58 +1,52 @@
 <?php
-// must be run within Dokuwiki
-if(!defined('DOKU_INC')) die();
-
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-require_once(DOKU_PLUGIN.'admin.php');
-
 /**
- * All DokuWiki plugins to extend the admin function
- * need to inherit from this class
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Andreas Gohr <andi@splitbrain.org>
  */
-class admin_plugin_ipban extends DokuWiki_Admin_Plugin {
 
-    /**
-     * access for managers
-     */
-    function forAdminOnly(){
+class admin_plugin_ipban extends DokuWiki_Admin_Plugin
+{
+
+    /** @inheritDoc */
+    function forAdminOnly()
+    {
         return false;
     }
 
-    /**
-     * return sort order for position in admin menu
-     */
-    function getMenuSort() {
+    /** @inheritDoc */
+    function getMenuSort()
+    {
         return 41;
     }
 
-    /**
-     * handle user request
-     */
-    function handle() {
+    /** @inheritDoc */
+    function handle()
+    {
         global $conf;
-        if(preg_match('/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/',trim($_REQUEST['ip']))){
-            $newban = trim($_REQUEST['ip'])."\t".time()."\t".$_SERVER['REMOTE_USER'];
-            $cause  = trim(preg_replace('/[\n\r\t]+/','',$_REQUEST['cause']));
-            $newban .= "\t".$cause."\n";
-            io_savefile($conf['cachedir'].'/ipbanplugin.txt',$newban,true);
+        if (preg_match('/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/', trim($_REQUEST['ip']))) {
+            $newban = trim($_REQUEST['ip']) . "\t" . time() . "\t" . $_SERVER['REMOTE_USER'];
+            $cause = trim(preg_replace('/[\n\r\t]+/', '', $_REQUEST['cause']));
+            $newban .= "\t" . $cause . "\n";
+            io_savefile($conf['cachedir'] . '/ipbanplugin.txt', $newban, true);
         }
 
-        if(is_array($_REQUEST['delip'])){
+        if (is_array($_REQUEST['delip'])) {
             $del = trim(array_shift(array_keys($_REQUEST['delip'])));
-            $del = preg_quote($del,'/');
+            $del = preg_quote($del, '/');
             $new = array();
-            $bans = @file($conf['cachedir'].'/ipbanplugin.txt');
-            if(is_array($bans)) foreach($bans as $ban){
-                if(!preg_match('/^'.$del.'\t/',$ban)) $new[] = $ban;
+            $bans = @file($conf['cachedir'] . '/ipbanplugin.txt');
+            if (is_array($bans)) {
+                foreach ($bans as $ban) {
+                    if (!preg_match('/^' . $del . '\t/', $ban)) $new[] = $ban;
+                }
             }
-            io_savefile($conf['cachedir'].'/ipbanplugin.txt',join('',$new));
+            io_savefile($conf['cachedir'] . '/ipbanplugin.txt', join('', $new));
         }
     }
 
-    /**
-     * output appropriate html
-     */
-    function html() {
+    /** @inheritDoc */
+    function html()
+    {
         global $conf;
 
         echo $this->locale_xhtml('intro');
@@ -60,35 +54,37 @@ class admin_plugin_ipban extends DokuWiki_Admin_Plugin {
         echo '<form method="post" action="">';
         echo '<table class="inline" width="100%">';
         echo '<tr>';
-        echo '<th>'.$this->getLang('ip').'</th>';
-        echo '<th>'.$this->getLang('host').'</th>';
-        echo '<th>'.$this->getLang('date').'</th>';
-        echo '<th>'.$this->getLang('by').'</th>';
-        echo '<th>'.$this->getLang('cause').'</th>';
-        echo '<th>'.$this->getLang('del').'</th>';
+        echo '<th>' . $this->getLang('ip') . '</th>';
+        echo '<th>' . $this->getLang('host') . '</th>';
+        echo '<th>' . $this->getLang('date') . '</th>';
+        echo '<th>' . $this->getLang('by') . '</th>';
+        echo '<th>' . $this->getLang('cause') . '</th>';
+        echo '<th>' . $this->getLang('del') . '</th>';
         echo '</tr>';
-        $bans = @file($conf['cachedir'].'/ipbanplugin.txt');
-        if(is_array($bans)) foreach($bans as $ban){
-            $fields = explode("\t",$ban);
-            echo '<tr>';
-            echo '<td>'.hsc($fields[0]).'</td>';
-            $host = @gethostbyaddr($fields[0]);
-            if(!$host || $host == $fields[0]) $host='?';
-            echo '<td>'.hsc($host).'</td>';
-            echo '<td>'.strftime($conf['dformat'],$fields[1]).'</td>';
-            echo '<td>'.hsc($fields[2]).'</td>';
-            echo '<td>'.hsc($fields[3]).'</td>';
-            echo '<td><input type="submit" name="delip['.$fields[0].']" value="'.$this->getLang('del').'" class="button" /></td>';
-            echo '</tr>';
+        $bans = @file($conf['cachedir'] . '/ipbanplugin.txt');
+        if (is_array($bans)) {
+            foreach ($bans as $ban) {
+                $fields = explode("\t", $ban);
+                echo '<tr>';
+                echo '<td>' . hsc($fields[0]) . '</td>';
+                $host = @gethostbyaddr($fields[0]);
+                if (!$host || $host == $fields[0]) $host = '?';
+                echo '<td>' . hsc($host) . '</td>';
+                echo '<td>' . strftime($conf['dformat'], $fields[1]) . '</td>';
+                echo '<td>' . hsc($fields[2]) . '</td>';
+                echo '<td>' . hsc($fields[3]) . '</td>';
+                echo '<td><input type="submit" name="delip[' . $fields[0] . ']" value="' . $this->getLang('del') . '" class="button" /></td>';
+                echo '</tr>';
+            }
         }
         echo '<tr>';
         echo '<th colspan="6">';
-        echo '<div>'.$this->getLang('newban').':</div>';
-        echo '<label for="plg__ipban_ip">'.$this->getLang('ip').':</label>';
+        echo '<div>' . $this->getLang('newban') . ':</div>';
+        echo '<label for="plg__ipban_ip">' . $this->getLang('ip') . ':</label>';
         echo '<input type="text" name="ip" id="plg__ipban_ip" class="edit" /> ';
-        echo '<label for="plg__ipban_cause">'.$this->getLang('cause').':</label>';
+        echo '<label for="plg__ipban_cause">' . $this->getLang('cause') . ':</label>';
         echo '<input type="text" name="cause" id="plg__ipban_cause" class="edit" /> ';
-        echo '<input type="submit" class="button" value="'.$this->getLang('ban').'" />';
+        echo '<input type="submit" class="button" value="' . $this->getLang('ban') . '" />';
         echo '</th>';
         echo '</tr>';
         echo '</table>';
@@ -96,6 +92,4 @@ class admin_plugin_ipban extends DokuWiki_Admin_Plugin {
 
     }
 
-
 }
-//Setup VIM: ex: et ts=4 enc=utf-8 :
