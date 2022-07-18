7455ce29552ff28cf7efddd10fef5647557b1645
钟龙浩 <mcptool@outlook.com>
2022-07-18 09:45:28 +0200

    translation update

---
+++ lang/zh/lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author 钟龙浩 <mcptool@outlook.com>
+ */
+$lang['menu']                  = '封禁IP管理';
+$lang['ip']                    = 'IP地址';
+$lang['date']                  = '日期';
+$lang['by']                    = '封禁者';
+$lang['cause']                 = '理由';
+$lang['del']                   = '删除';
+$lang['ban']                   = '封禁';
+$lang['newban']                = '添加封禁';
+$lang['badrange']              = '给定的 IP 范围无效，并且尚未添加。';
+$lang['denied']                = '访问被拒绝';
+$lang['banned']                = '您的IP（%s）在 %s 上被封禁，理由：<br /><i>%s</i>';
